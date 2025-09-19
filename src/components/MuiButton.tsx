f3efd8e0415ad7a4492cb73b1fc3821d543009bb
alvish-arforgetech <alvish.shrestha@arforgetech.com>
2025-09-19 12:02:14 +0545

    toggle button

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,8 +1,26 @@
-import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
+import { useState } from "react";
 
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 
 export const MuiButton = () => {
+  const [formats, setFormats] = useState<string[]>([]);
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string[]
+  ) => {
+    setFormats(updatedFormats);
+  };
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction="row">
@@ -33,6 +51,7 @@ export const MuiButton = () => {
           Success
         </Button>
       </Stack>
+
       <Stack display={"block"} spacing={2} direction={"row"}>
         <Button variant="contained" size="small">
           Small
@@ -44,6 +63,7 @@ export const MuiButton = () => {
           Large
         </Button>
       </Stack>
+
       <Stack spacing={2} direction={"row"}>
         <Button
           variant="contained"
@@ -60,6 +80,7 @@ export const MuiButton = () => {
           <SendIcon />
         </IconButton>
       </Stack>
+
       <Stack direction={"row"}>
         <ButtonGroup
           variant="contained"
@@ -72,6 +93,27 @@ export const MuiButton = () => {
           <Button onClick={() => alert("Center clicked")}>Center</Button>
           <Button onClick={() => alert("Right clicked")}>Right</Button>
         </ButtonGroup>
+      </Stack>
+
+      <Stack direction={"row"}>
+        <ToggleButtonGroup
+          aria-label="text formating"
+          value={formats}
+          onChange={handleFormatChange}
+          size="small"
+          color="success"
+          exclusive
+        >
+          <ToggleButton value={"bold"} aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value={"italic"} aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value={"underlined"} aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
   );
