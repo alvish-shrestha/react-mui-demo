c8eb8304e97f54b84f4f9484c55205fb1b78bf77
alvish-arforgetech <alvish.shrestha@arforgetech.com>
2025-09-21 14:15:57 +0545

    grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,35 +1,60 @@
-import { Box, Divider, Stack } from "@mui/material";
+import { Box, Divider, Grid, Stack } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <Stack
-      sx={{ border: "1px solid" }}
-      direction={"row"}
-      spacing={2}
-      divider={<Divider orientation="vertical" flexItem />}
-    >
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          color: "white",
-          height: "100px",
-          width: "100px",
-          padding: "16px",
-          "&:hover": {
-            backgroundColor: "primary.light",
-          },
-        }}
+    <>
+      <Stack
+        sx={{ border: "1px solid" }}
+        direction={"row"}
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
       >
-        Arforgetech
-      </Box>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            color: "white",
+            height: "100px",
+            width: "100px",
+            padding: "16px",
+            "&:hover": {
+              backgroundColor: "primary.light",
+            },
+          }}
+        >
+          Arforgetech
+        </Box>
 
-      <Box
-        display={"flex"}
-        height={"100px"}
-        width={"100px"}
-        bgcolor={"success.light"}
-        p={2}
-      ></Box>
-    </Stack>
+        <Box
+          display={"flex"}
+          height={"100px"}
+          width={"100px"}
+          bgcolor={"success.light"}
+          p={2}
+        ></Box>
+      </Stack>
+
+      <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+        <Grid>
+          <Box bgcolor="primary.light" p={2}>
+            Item 1
+          </Box>
+        </Grid>
+        <Grid>
+          <Box bgcolor="primary.light" p={2}>
+            Item 2
+          </Box>
+        </Grid>
+        <Grid>
+          <Box bgcolor="primary.light" p={2}>
+            Item 3
+          </Box>
+        </Grid>
+        <Grid>
+          <Box bgcolor="primary.light" p={2}>
+            Item 4
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 };
